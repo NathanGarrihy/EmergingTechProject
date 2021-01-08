e1ae9267665577a637676bd0845829a894fc004b
NathanGarrihy <37175360+NathanGarrihy@users.noreply.github.com>
2021-01-08 19:10:45 +0000

    Tidied up project

--- WebService.py
+++ WebService.py
@@ -1,26 +1,25 @@
 from flask import Flask, jsonify, request
 import json
 import tensorflow as tflow
+# https://blog.keras.io/building-a-simple-keras-deep-learning-rest-api.html
 
 app = Flask(__name__)
 
+# Sets route index of app
 @app.route('/')
 def parseIndex():
     return app.send_static_file('index.html')
 
-@app.route('/index', methods=['GET'])
-def parseGet():
-    return app.send_static_file('index.html')
-
+# takes speed data and uses model to calculate power
 @app.route('/process/pow', methods=['POST'])
 def parsePost():
     data = request.data
     myJson = json.loads(data)
 
-    wSpeed = float(myJson['speed'])
+    wSpeedValue = float(myJson['wSpeedValue'])
     model = tflow.keras.models.load_model('predictionModel.h5')
 
-    prediction = model.predict([wSpeed])
+    prediction = model.predict([wSpeedValue])
     flatten = prediction.flatten()
-    print(flatten[0])
+
     return str(flatten[0]) 
